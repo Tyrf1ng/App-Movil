7139b0fd230018c84bf31a734d3f3081199caeb0
Roussd <aracelli.rivas2101@alumnos.ubiobio.cl>
2025-01-24 13:30:10 -0300

    Refactor Firebase configuration to use Expo-specific environment variables

--- config/firebase.js
+++ config/firebase.js
@@ -4,23 +4,23 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { 
-    FIREBASE_API_KEY, 
-    FIREBASE_AUTH_DOMAIN, 
-    FIREBASE_DATABASE_URL, 
-    FIREBASE_PROJECT_ID, 
-    FIREBASE_STORAGE_BUCKET, 
-    FIREBASE_MESSAGING_SENDER_ID, 
-    FIREBASE_APP_ID 
+    EXPO_FIREBASE_API_KEY, 
+    EXPO_FIREBASE_AUTH_DOMAIN, 
+    EXPO_FIREBASE_DATABASE_URL, 
+    EXPO_FIREBASE_PROJECT_ID, 
+    EXPO_FIREBASE_STORAGE_BUCKET, 
+    EXPO_FIREBASE_MESSAGING_SENDER_ID, 
+    EXPO_FIREBASE_APP_ID 
 } from '@env';
 
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    databaseURL: FIREBASE_DATABASE_URL,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID
+    apiKey: EXPO_FIREBASE_API_KEY,
+    authDomain: EXPO_FIREBASE_AUTH_DOMAIN,
+    databaseURL: EXPO_FIREBASE_DATABASE_URL,
+    projectId: EXPO_FIREBASE_PROJECT_ID,
+    storageBucket: EXPO_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: EXPO_FIREBASE_MESSAGING_SENDER_ID,
+    appId: EXPO_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
